1f5d7c25fa9efb9f305fdcd00004eeb38f6f8c3e
Jakob Jarosch <dev@jakobjarosch.de>
2020-04-06 20:36:35 +0200

    remove first header (the black one)

--- lib/header-background.php
+++ lib/header-background.php
@@ -4,7 +4,7 @@ function dpsgnt_header_background() {
 ?>
 <style>
 .site-header {
-    background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/header-background/background-<?php echo rand(1,4); ?>.jpg);
+    background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/header-background/background-<?php echo rand(1,3); ?>.jpg);
 }
 </style>
 <?php
